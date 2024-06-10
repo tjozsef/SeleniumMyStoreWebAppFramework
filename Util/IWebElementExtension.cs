d855aac03d541fb115b1459b6a6b49b994bdcd19
tjozsef <22714488+tjozsef@users.noreply.github.com>
2024-06-10 16:22:24 +0200

    Format files

--- Util/IWebElementExtension.cs
+++ Util/IWebElementExtension.cs
@@ -3,52 +3,64 @@ using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumMyStoreWebAppTest.Util;
 
-public static class IWebElementExtension{
+public static class IWebElementExtension
+{
 
-    public static void Clicks(this IWebElement locator){
+    public static void Clicks(this IWebElement locator)
+    {
         locator.Click();
     }
 
-    public static void Submits(this IWebElement locator){
+    public static void Submits(this IWebElement locator)
+    {
         locator.Submit();
     }
 
-    public static void EnterText(this IWebElement locator, string text){
+    public static void EnterText(this IWebElement locator, string text)
+    {
         locator.Clear();
         locator.SendKeys(text);
     }
 
-    public static void SelectDropDownByText(this IWebElement locator, string text){
-        var selectElement= new SelectElement(locator);
+    public static void SelectDropDownByText(this IWebElement locator, string text)
+    {
+        var selectElement = new SelectElement(locator);
         selectElement.SelectByText(text);
     }
 
-        public static void SelectDropDownByValue(this IWebElement locator, string value){
-        var selectElement= new SelectElement(locator);
+    public static void SelectDropDownByValue(this IWebElement locator, string value)
+    {
+        var selectElement = new SelectElement(locator);
         selectElement.SelectByValue(value);
     }
 
-    public static void SelectMultipleDropDownElementsByText(this IWebElement locator, string[] texts){
+    public static void SelectMultipleDropDownElementsByText(this IWebElement locator, string[] texts)
+    {
         var selectElement = new SelectElement(locator);
-        foreach(var text in texts){
+        foreach (var text in texts)
+        {
             selectElement.SelectByText(text);
         }
     }
 
 
-    public static void SelectMultipleDropDownElementsByValue(this IWebElement locator, string[] values){
+    public static void SelectMultipleDropDownElementsByValue(this IWebElement locator, string[] values)
+    {
         var selectElement = new SelectElement(locator);
-        foreach(var value in values){
+        foreach (var value in values)
+        {
             selectElement.SelectByValue(value);
         }
     }
 
-    public static List<string> GetAllSelectedDropDownTexts(this IWebElement locator){
+    public static List<string> GetAllSelectedDropDownTexts(this IWebElement locator)
+    {
         var selectedTexts = new List<string>();
         var selectElement = new SelectElement(locator);
 
         var allSelectedOptions = selectElement.AllSelectedOptions;
-        foreach(IWebElement selectedOption in allSelectedOptions){
+        foreach (IWebElement selectedOption in allSelectedOptions)
+        {
             selectedTexts.Add(selectedOption.Text);
         }
         return selectedTexts;
--- Util/Reporter.cs
+++ Util/Reporter.cs
@@ -1,25 +1,27 @@
-
 using AventStack.ExtentReports;
 using AventStack.ExtentReports.Reporter;
 
 namespace SeleniumMyStoreWebAppTest.Util;
 
-public static class Reporter{
+public static class Reporter
+{
 
-    private static ExtentReports _extentReports=new();
+    private static ExtentReports _extentReports = new();
 
 
-    public static void AttachReporter(string fileName){
-            var currentDirectory = Environment.CurrentDirectory;
-            var projectRootDirectory = Directory.GetParent(currentDirectory)?.Parent?.Parent?.FullName??"";
-            var reportDirectory = Path.Combine(projectRootDirectory,"Report");
-            var reporter = new ExtentSparkReporter(Path.Combine(reportDirectory, fileName));
-            _extentReports.AttachReporter(reporter);
+    public static void AttachReporter(string fileName)
+    {
+        var currentDirectory = Environment.CurrentDirectory;
+        var projectRootDirectory = Directory.GetParent(currentDirectory)?.Parent?.Parent?.FullName ?? "";
+        var reportDirectory = Path.Combine(projectRootDirectory, "Report");
+        var reporter = new ExtentSparkReporter(Path.Combine(reportDirectory, fileName));
+        _extentReports.AttachReporter(reporter);
     }
 
-    public static void FlushReporters(){
+    public static void FlushReporters()
+    {
         _extentReports.Flush();
     }
 
-    public static ExtentReports Extent =>_extentReports;
+    public static ExtentReports Extent => _extentReports;
 }
